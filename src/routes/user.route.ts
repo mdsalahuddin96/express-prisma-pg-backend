911468626bc128106eeb95cf85541f5dea5c492f
mdsalauddin96 <salauddincse96@gmail.com>
2026-08-10 01:07:11 +0600

    create user update delete and get api end point

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../services/user.service";
+import { createUser, deleteUser, getAllUser, updateUser } from "../services/user.service";
 const route=Router()
 
 route.post("/create",createUser)
-
+route.get("/",getAllUser)
+route.patch("/update",updateUser)
+route.delete("/delete",deleteUser)
 export default route;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import prisma from "../lib/prisma";
 
-export const createUser = async (req:Request, res:Response) => {
+export const createUser = async (req: Request, res: Response) => {
   const userData = req.body;
   try {
-    const data = await prisma.user.create({data:userData});
+    const newUser = await prisma.user.create({ data: userData });
     res.status(201).json({
       success: true,
       message: "User Created Successfully",
-      data,
+      data: newUser,
     });
   } catch (error) {
     console.error(error);
@@ -19,3 +19,69 @@ export const createUser = async (req:Request, res:Response) => {
     });
   }
 };
+
+export const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const allUsers = await prisma.user.findMany();
+    res.status(200).json({
+      success: true,
+      message: "Get All User Successfully",
+      data: allUsers,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error instanceof Error ? error.message : "Something went wrong",
+    });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  const newData = req.body;
+  try {
+    const updateUser = await prisma.user.update({
+      where: {
+        email: "rahim@example.com",
+      },
+      data: newData,
+    });
+    res.status(201).json({
+      success:true,
+      message:'User updated successfully',
+      data:updateUser
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error instanceof Error ? error.message : "Something went wrong",
+    });
+  }
+};
+
+export const deleteUser=async(req:Request,res:Response)=>{
+  const email=req.query.email;
+  try{
+    const deletedUser=await prisma.user.update({
+      where:{
+        email:email?.toString()
+      },
+      data:{
+        deletedAt:new Date()
+      },
+    })
+    res.status(200).json({
+      success:true,
+      message:'User deleted successfully',
+      data:deletedUser
+    })
+  }
+  catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error instanceof Error ? error.message : "Something went wrong",
+    });
+  }
+}
